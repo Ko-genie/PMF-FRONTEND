3d4b66e5f21db7f55f76e1ebdf68b2bba2726468
aaviix <singhavanish8642@gmail.com>
2024-10-24 18:28:10 +0530

    url updated

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -68,7 +68,7 @@ const OrganizationIdPage = () => {
     }
 
     try {
-      const response = await fetch("https://server-eta-lovat.vercel.app/createAd", {
+      const response = await fetch("https://kogenie-server-657156012235.us-central1.run.app/createAd", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url, gender, ageGroup }), // Send gender and ageGroup along with the URL
