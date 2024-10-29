0972410cd06c1ade34c83bfee88a6bda67431d06
aaviix <singhavanish8642@gmail.com>
2024-10-29 13:20:22 +0530

    updated backend url

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -68,7 +68,7 @@ const OrganizationIdPage = () => {
     }
 
     try {
-      const response = await fetch("https://kogenie-server-58c14ee2f1ed.herokuapp.com/createAd", {
+      const response = await fetch("https://kogenie-server-f078f630bb40.herokuapp.com/createAd", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url, gender, ageGroup }), // Send gender and ageGroup along with the URL
