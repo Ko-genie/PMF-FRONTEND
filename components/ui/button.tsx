ffe1d97b5084bac3fac72346a001cfbd5f298e7d
aaviix <singhavanish8642@gmail.com>
2024-11-11 17:08:19 +0530

    first commit

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-[#DB4A2B] text-primary-foreground hover:bg-[#DB4A2B]/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
